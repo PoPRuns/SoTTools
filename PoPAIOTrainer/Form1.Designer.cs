319137d8f42506555a1474b7a79f49466ca00b34
GMP <pranavbtech@gmail.com>
2021-10-09 14:12:14 +0530

    Add project files.

---
+++ PoPAIOTrainer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace PoPAIOTrainer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pidLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Detected: ";
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.AutoSize = true;
+            this.gameLabel.Location = new System.Drawing.Point(106, 37);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(23, 13);
+            this.gameLabel.TabIndex = 1;
+            this.gameLabel.Text = "NO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Process ID:";
+            // 
+            // pidLabel
+            // 
+            this.pidLabel.AutoSize = true;
+            this.pidLabel.Location = new System.Drawing.Point(106, 59);
+            this.pidLabel.Name = "pidLabel";
+            this.pidLabel.Size = new System.Drawing.Size(13, 13);
+            this.pidLabel.TabIndex = 3;
+            this.pidLabel.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prince of Persia: The Sands of Time - Hard Mode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 92);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pidLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gameLabel);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PoP:SoT HM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label pidLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PoPAIOTrainer/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace PoPAIOTrainer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public Mem m = new Mem();
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (!backgroundWorker1.IsBusy)
+				backgroundWorker1.RunWorkerAsync();
+		}
+
+		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+		{
+			int pid = 0;
+			while (true)
+			{
+				bool openproc = false;
+
+				if (m.GetProcIdFromName("pop") > 0)
+                {
+					pid = m.GetProcIdFromName("pop");
+					openproc = m.OpenProcess(pid);
+					gameLabel.Invoke((MethodInvoker)delegate
+					{
+						gameLabel.Text = "YES";
+					});
+					pidLabel.Invoke((MethodInvoker)delegate
+					{
+						pidLabel.Text = pid.ToString();
+					});
+					m.WriteMemory("base+0x006db8f4,0x0", "int", "0");	// Max Sands set to 0
+					m.WriteMemory("base+0x006db8f4,0x4d4", "int", "1");	// Max HP set to 1
+					m.WriteMemory("base+0x006db8a4,0x584", "int", "0");	// Max Moons set to 0
+					if (m.ReadInt("base+006DB500,0x0") > 1)				// If HP > 1, it is capped to 1
+                    {
+						m.WriteMemory("base+006DB500,0x0", "int", "1");
+					}
+				}
+				else
+                {
+					gameLabel.Invoke((MethodInvoker)delegate
+					{
+						gameLabel.Text = "NO";
+					});
+					pidLabel.Invoke((MethodInvoker)delegate
+					{
+						pidLabel.Text = "?";
+					});
+				}
+			}
+		}
+    }
+}
